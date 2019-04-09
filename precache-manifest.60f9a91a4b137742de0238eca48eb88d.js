51e8506c5828cb893633bd3fb80942ca992b1203
Yorvin Rosario <yorvin.rosario@axa-equitable.com>
2019-04-09 14:32:42 -0400

    Updates

---
+++ precache-manifest.60f9a91a4b137742de0238eca48eb88d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "64d35187bc540f5214de",
+    "url": "/tic-tac-toe/static/js/main.e32db715.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "64d35187bc540f5214de",
+    "url": "/tic-tac-toe/static/css/main.03365b72.chunk.css"
+  },
+  {
+    "revision": "d51b84153fd40fe929c5114026bbd6e6",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
